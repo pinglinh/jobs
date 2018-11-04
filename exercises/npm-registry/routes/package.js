b1ec49c1559dc00d12526db2dd35884ef82713c5
pinglinh <lnguyenmy1@gmail.com>
2018-11-04 17:39:11 +0000

    Update package.js

--- exercises/npm-registry/routes/package.js
+++ exercises/npm-registry/routes/package.js
@@ -5,12 +5,19 @@ const router = express.Router();
 /* GET package information. */
 router.get('/:packageName/:packageVersion?', async (req, res, next) => {
   const { params } = req;
+  // const params = req.params;
   const packageVersion = params.packageVersion || 'latest';
+
+
+  let dependencyPackageName = Object.keys(data.dependencies);
+  let dependencyPackageVersion = Object.values(data.dependencies);
 
   const npmRes = await request(`https://registry.npmjs.org/${params.packageName}/${packageVersion}`, {
     resolveWithFullResponse: true,
     json: true,
-  });
+  })
+    .then.map(data => 
+      request(`https://registry.npmjs.org/${dependencyPackageName}/${dependencyPackageVersion}`))
 
   const { name, version, dependencies } = npmRes.body;
 
@@ -18,7 +25,7 @@ router.get('/:packageName/:packageVersion?', async (req, res, next) => {
     .json({
       name,
       version,
-      dependencies,
+      dependencies
     });
 });
 
