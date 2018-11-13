2538b7abca443b467c9a03dfefcd362ba864e8c2
pinglinh <lnguyenmy1@gmail.com>
2018-11-13 00:09:36 +0000

    Add test for transitive dependencies

--- exercises/npm-registry/test/system/package.test.js
+++ exercises/npm-registry/test/system/package.test.js
@@ -1,24 +1,50 @@
-const test = require('tap').test;
-const server = require('../fixtures/test-server');
-const request = require('request-promise-native');
+const test = require("tap").test;
+const server = require("../fixtures/test-server");
+const request = require("request-promise-native");
 
-test('setup', async (t) => {
+const responseForCssPackage = [
+  ["inherits", "2.0.3"],
+  ["source-map", "0.6.1"],
+  ["source-map-resolve", "0.5.2"],
+  ["urix", "0.1.0"],
+  ["atob", "2.1.1"],
+  ["decode-uri-component", "0.2.0"],
+  ["resolve-url", "0.2.1"],
+  ["source-map-url", "0.4.0"],
+  ["urix", "0.1.0"]
+];
+
+test("setup", async t => {
   await server.start();
-  t.pass('server available');
+  t.pass("server available");
 });
 
-test('/package/snyk responds', async (t) => {
-  const res = await request('http://localhost:4545/package/snyk', {
-    resolveWithFullResponse: true,
+test("/package/css responds", async t => {
+  const res = await request("http://localhost:3000/package/css/2.2.4", {
+    resolveWithFullResponse: true
   });
 
-  t.equal(res.statusCode, 200,
-    'returns successful statuscode');
-  t.equal(res.headers['content-type'], 'application/json; charset=utf-8',
-    'correct content type returned');
+  t.equal(res.statusCode, 200, "returns successful statuscode");
+  t.equal(
+    res.headers["content-type"],
+    "application/json; charset=utf-8",
+    "correct content type returned"
+  );
 });
 
+test("/package/css gets transitive dependencies", async t => {
+  const res = await request("http://localhost:3000/package/css/2.2.4", {
+    resolveWithFullResponse: true,
+    json: true
+  });
 
-test('teardown', async () => {
+  console.log("this is res.json", res.json);
+
+  t.equal(res.json, responseForCssPackage, "returns transitive dependency");
+
+  console.log("this is res.json 2", res.json);
+});
+
+test("teardown", async () => {
   server.stop();
 });
